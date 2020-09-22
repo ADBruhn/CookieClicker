4f7fae2f16969e953c0116206aae75d33c52c055
Ari Bruhn <bruhnad@s.dcsdk12.org>
2020-09-22 09:15:56 -0600

    Initial

---
+++ CookieClicker.js
@@ -0,0 +1,11 @@
+let cookieEl = document.getElementById('cookie')
+let cookieCountEl = get
+let countChange = 1
+let showButtonEl = document.getElementById ('showButton')
+let removeButtonEl = document.getElementById ('removeButton')
+
+
+function (click) {
+    cookieCountEl = cookieCountEl + countChange
+
+}
